f5772a2f059f997d0c82c48a7a150374bfef8e2d
Jo <10510431+j178@users.noreply.github.com>
2024-12-27 11:18:56 +0800

    Try to use D drive on Windows CI (#157)
    * Try to use D drive on Windows CI
    
    * Simplify
    
    * CI cache

--- .github/workflows/setup-dev-drive.ps1
+++ .github/workflows/setup-dev-drive.ps1
@@ -1,16 +1,16 @@
 # This creates a 10GB dev drive, and exports all required environment
 # variables so that rustup, prefligit and others all use the dev drive as much
 # as possible.
-$Volume = New-VHD -Path C:/prefligit_dev_drive.vhdx -SizeBytes 10GB |
-					Mount-VHD -Passthru |
-					Initialize-Disk -Passthru |
-					New-Partition -AssignDriveLetter -UseMaximumSize |
-					Format-Volume -FileSystem ReFS -Confirm:$false -Force
+# $Volume = New-VHD -Path C:/prefligit_dev_drive.vhdx -SizeBytes 10GB |
+# 					Mount-VHD -Passthru |
+# 					Initialize-Disk -Passthru |
+# 					New-Partition -AssignDriveLetter -UseMaximumSize |
+# 					Format-Volume -FileSystem ReFS -Confirm:$false -Force
+#
+# Write-Output $Volume
 
-Write-Output $Volume
-
-$Drive = "$($Volume.DriveLetter):"
-$Tmp = "$($Drive)/prefligit-tmp"
+$Drive = "D:"
+$Tmp = "$($Drive)\prefligit-tmp"
 
 # Create the directory ahead of time in an attempt to avoid race-conditions
 New-Item $Tmp -ItemType Directory
@@ -22,5 +22,4 @@ Write-Output `
 	"PREFLIGIT_INTERNAL__TEST_DIR=$($Tmp)" `
 	"RUSTUP_HOME=$($Drive)/.rustup" `
 	"CARGO_HOME=$($Drive)/.cargo" `
-	"PREFLIGIT_WORKSPACE=$($Drive)/prefligit" `
 	>> $env:GITHUB_ENV
