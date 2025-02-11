2f500edb8cb2ef045d750b1f3f8af5d8fb8bbb87
Jo <10510431+j178@users.noreply.github.com>
2025-02-11 12:00:03 +0800

    Format sample config output (#172)

--- src/cli/sample_config.rs
+++ src/cli/sample_config.rs
@@ -4,13 +4,13 @@ static SAMPLE_CONFIG: &str = "\
 # See https://pre-commit.com for more information
 # See https://pre-commit.com/hooks.html for more hooks
 repos:
--   repo: https://github.com/pre-commit/pre-commit-hooks
+  - repo: 'https://github.com/pre-commit/pre-commit-hooks'
     rev: v5.0.0
     hooks:
-    -   id: trailing-whitespace
-    -   id: end-of-file-fixer
-    -   id: check-yaml
-    -   id: check-added-large-files
+      - id: trailing-whitespace
+      - id: end-of-file-fixer
+      - id: check-yaml
+      - id: check-added-large-files
 ";
 
 #[allow(clippy::print_stdout)]
