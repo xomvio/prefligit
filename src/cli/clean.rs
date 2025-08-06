95ed5cd36a1f4b28875138b32c3073c049044569
Jo <10510431+j178@users.noreply.github.com>
2025-08-06 19:03:31 +0800

    Fix cache dir permission before clean (#368)
    * Fix cache dir permission before clean
    
    * fix windows
    
    * fix windows

--- src/cli/clean.rs
+++ src/cli/clean.rs
@@ -1,7 +1,10 @@
 use std::fmt::Write;
+use std::io;
+use std::path::Path;
 
 use anyhow::Result;
 use owo_colors::OwoColorize;
+use tracing::error;
 
 use crate::cli::ExitStatus;
 use crate::fs::Simplified;
@@ -16,6 +19,10 @@ pub(crate) fn clean(printer: Printer) -> Result<ExitStatus> {
         return Ok(ExitStatus::Success);
     }
 
+    if let Err(e) = fix_permissions(store.path()) {
+        error!("Failed to fix permissions: {}", e);
+    }
+
     fs_err::remove_dir_all(store.path())?;
     writeln!(
         printer.stdout(),
@@ -24,4 +31,41 @@ pub(crate) fn clean(printer: Printer) -> Result<ExitStatus> {
     )?;
 
     Ok(ExitStatus::Success)
+}
+
+/// Add write permission to GOMODCACHE directory recursively.
+/// Go sets the permissions to read-only by default.
+#[cfg(not(windows))]
+pub fn fix_permissions<P: AsRef<Path>>(path: P) -> io::Result<()> {
+    use std::fs;
+    use std::os::unix::fs::PermissionsExt;
+
+    let path = path.as_ref();
+    let metadata = fs::metadata(path)?;
+
+    let mut permissions = metadata.permissions();
+    let current_mode = permissions.mode();
+
+    // Add write permissions for owner, group, and others
+    let new_mode = current_mode | 0o222;
+    permissions.set_mode(new_mode);
+    fs::set_permissions(path, permissions)?;
+
+    // If it's a directory, recursively process its contents
+    if metadata.is_dir() {
+        let entries = fs::read_dir(path)?;
+        for entry in entries {
+            let entry = entry?;
+            fix_permissions(entry.path())?;
+        }
+    }
+
+    Ok(())
+}
+
+#[cfg(windows)]
+#[allow(clippy::unnecessary_wraps)]
+pub fn fix_permissions<P: AsRef<Path>>(_path: P) -> io::Result<()> {
+    // On Windows, permissions are handled differently and this function does nothing.
+    Ok(())
 }
